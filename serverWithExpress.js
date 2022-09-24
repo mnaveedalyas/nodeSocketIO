221c03022e374182b2729c4d1b57c3c5419f5deb
mnaveedalyas <naveed@terminustech.co.nz>
2022-09-24 12:16:21 +1200

    SocketIO and Exporess

--- serverWithExpress.js
+++ serverWithExpress.js
@@ -1,12 +1,27 @@
-const server = require('http').createServer();
+//const server = require('http').createServer();
+//const io = require('socket.io')(server);
+
+const app = require('express')();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-//let port = 3000;
 server.listen(3000);
 
-console.log("opening a stocket : 3000 ");
+console.log("opening a Web Server at http://localhost:3000/ ");
+//let port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.send("<h1>App is running </h1>");
+})
+
+
+
 
 io.on("connection", (socket) => {
+    console.log("opening a stocket at :3000 ");
     //send a message to client
     socket.emit("Topic1", "Weather Information");
 
